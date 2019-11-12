2d378c0cd0d0142d7999f4dcf7df2edab7bcb65a
Nathan del Carmen <Nathan@Nathans-MacBook-Pro.local>
2019-11-11 20:29:49 -0500

    florida work

---
+++ Assets/Scripts/Bob.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob : MonoBehaviour
+{
+    public float speed;
+    public float height;
+
+    void Start(){
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get the objects current position and put it in a variable so we can access it later with less code
+        
+        Vector3 pos = transform.localPosition;
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed) * height;
+        //Debug.Log(newY);
+        //set the object's Y to the new calculated Y
+        //Debug.Log(new Vector3(pos.x, newY, pos.z) * height);
+        this.transform.localPosition = new Vector3(pos.x, newY, pos.z);
+    }
+}
